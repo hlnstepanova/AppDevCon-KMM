57d25703926ba9652c1ad39dd3d05d896dba09b8
Lena Stepanova <hlnstepanova@yandex.ru>
2023-05-06 12:31:35 +0200

    update starter project
    * update starter project

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     id("com.android.application")
     kotlin("android")
@@ -24,26 +23,38 @@ android {
         compose = true
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.4"
+    }
+    namespace = "dev.tutorial.kmmargherita.android"
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
 dependencies {
     implementation(project(":shared"))
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Koin
-    implementation("io.insert-koin:koin-android:3.2.0")
+    implementation("io.insert-koin:koin-android:3.3.2")
 
     // Jetpack Compose
-    implementation("androidx.compose.ui:ui:1.4.2")
-    implementation("androidx.compose.ui:ui-graphics:1.4.2")
-    implementation("androidx.compose.ui:ui-tooling:1.4.2")
-    implementation("androidx.compose.foundation:foundation-layout:1.4.2")
-    implementation("androidx.compose.material:material:1.4.2")
+    implementation("androidx.compose.ui:ui:1.4.3")
+    implementation("androidx.compose.ui:ui-graphics:1.4.3")
+    implementation("androidx.compose.ui:ui-tooling:1.4.3")
+    implementation("androidx.compose.foundation:foundation-layout:1.4.3")
+    implementation("androidx.compose.material:material:1.4.3")
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.activity:activity-compose:1.7.1")
 }
