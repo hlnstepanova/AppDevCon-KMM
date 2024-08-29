60020b4ac23f7746e42c3dc79045f7a1d20f590b
Lena Stepanova <elena.stepanova@scalable.capital>
2024-08-29 13:57:24 +0200

    add koin config

--- shared/src/commonMain/kotlin/dev/tutorial/kmmargherita/remote/RecipeRemoteSource.kt
+++ shared/src/commonMain/kotlin/dev/tutorial/kmmargherita/remote/RecipeRemoteSource.kt
@@ -5,5 +5,5 @@ import dev.tutorial.kmmargherita.api.RecipesApi
 class RecipeRemoteSource(
     private val recipesApi: RecipesApi,
 ) {
-    suspend fun getRecipes() = {}
+    suspend fun getRecipes() = recipesApi.getRecipes()
 }
--- shared/src/commonMain/kotlin/dev/tutorial/kmmargherita/repository/RecipeRepository.kt
+++ shared/src/commonMain/kotlin/dev/tutorial/kmmargherita/repository/RecipeRepository.kt
@@ -6,5 +6,7 @@ import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class RecipeRepository : KoinComponent {
+    private val recipeRemoteSource: RecipeRemoteSource by inject()
 
+    suspend fun getRecipes(): List<Recipe> = recipeRemoteSource.getRecipes()
 }
