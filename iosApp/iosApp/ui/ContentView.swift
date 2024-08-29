5ffb0908a10ed51bdf34574d778159eac131c036
Lena Stepanova <elena.stepanova@scalable.capital>
2024-08-29 14:24:39 +0200

    add main view

--- iosApp/iosApp/ui/ContentView.swift
+++ iosApp/iosApp/ui/ContentView.swift
@@ -2,11 +2,11 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-    let greet = "Hello"
-
-
+    
     var body: some View {
-        Text(greet)
+            VStack {
+                RecipesView()
+            }
     }
 }
 
